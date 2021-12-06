4a9bf9c136868f69b0ad190c225835049dbdf040
Dzmitry Mikhalapau <dzmitry.mikhalapau@gmail.com>
2021-12-06 15:45:34 +0600

    Resolve package version dynamically based on git hash

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
+import importlib.metadata
+
 from click.testing import CliRunner
 
-from arq.cli import cli
+import arq.cli
 
 
 async def foobar(ctx):
@@ -12,23 +14,39 @@ class WorkerSettings:
     functions = [foobar]
 
 
+def test__get_version(mocker):
+    mocker.patch('importlib.metadata.version').return_value = 'v0.0.1b1'
+
+    assert arq.cli._get_version() == 'v0.0.1b1'
+    importlib.metadata.version.assert_called_once_with('arq')
+
+
+def test__get_version_package_does_not_exist(mocker):
+    mocker.patch(
+        'importlib.metadata.version',
+    ).side_effect = importlib.metadata.PackageNotFoundError
+
+    assert arq.cli._get_version() == 'unknown'
+    importlib.metadata.version.assert_called_once_with('arq')
+
+
 def test_help():
     runner = CliRunner()
-    result = runner.invoke(cli, ['--help'])
+    result = runner.invoke(arq.cli.cli, ['--help'])
     assert result.exit_code == 0
     assert result.output.startswith('Usage: arq [OPTIONS] WORKER_SETTINGS\n')
 
 
 def test_run():
     runner = CliRunner()
-    result = runner.invoke(cli, ['tests.test_cli.WorkerSettings'])
+    result = runner.invoke(arq.cli.cli, ['tests.test_cli.WorkerSettings'])
     assert result.exit_code == 0
     assert 'Starting worker for 1 functions: foobar' in result.output
 
 
 def test_check():
     runner = CliRunner()
-    result = runner.invoke(cli, ['tests.test_cli.WorkerSettings', '--check'])
+    result = runner.invoke(arq.cli.cli, ['tests.test_cli.WorkerSettings', '--check'])
     assert result.exit_code == 1
     assert 'Health check failed: no health check sentinel value found' in result.output
 
@@ -40,6 +58,6 @@ async def mock_awatch():
 def test_run_watch(mocker):
     mocker.patch('watchgod.awatch', return_value=mock_awatch())
     runner = CliRunner()
-    result = runner.invoke(cli, ['tests.test_cli.WorkerSettings', '--watch', 'tests'])
+    result = runner.invoke(arq.cli.cli, ['tests.test_cli.WorkerSettings', '--watch', 'tests'])
     assert result.exit_code == 0
     assert '1 files changes, reloading arq worker...'
